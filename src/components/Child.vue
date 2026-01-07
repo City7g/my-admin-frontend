ff65d79c58dc2e36c31e9b17a2aa14c16cadb709
Dima Mironenko <City7gor@gmail.com>
2026-01-07 16:06:44 +0200

    Add Parent and Child

---
+++ src/components/Child.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts" generic="T, K extends keyof T">
+const props = defineProps<{
+  item: T
+  field: K
+}>()
+</script>
+
+<template>
+  <div>{{ item }} - {{ field }}</div>
+</template>
---
+++ src/components/Parent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Child from './Child.vue'
+
+interface User {
+  id: number
+  name: string
+}
+
+// const users: User[] = [{ id: 1, name: 'Dima' }]
+const user = { id: 1, name: 'Dima' }
+</script>
+
+<template>
+  <Child :item="user" field="name" />
+</template>
