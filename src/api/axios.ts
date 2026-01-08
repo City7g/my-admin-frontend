7a1e8cb163c71c554c4cad9456118b50aa50e818
Dima Mironenko <City7gor@gmail.com>
2026-01-08 15:24:37 +0200

    Create axios instance

---
+++ src/api/axios.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use(
+  (config) => new Promise(resolve => setTimeout(() => resolve(config), 2000))
+)
+
+export default api
--- src/api/users.ts
+++ src/api/users.ts
@@ -1,9 +1,8 @@
 import type { Resource, User } from '@/types'
-import axios from 'axios'
+import api from './axios'
 
 const loadUsers = async () => {
-  await new Promise(resolve => setTimeout(resolve, +import.meta.env.VITE_TIMEOUT ?? 0))
-  const { data } = await axios.get<Resource<User>>(`${import.meta.env.VITE_API_URL}/users`)
+  const { data } = await api.get<Resource<User>>(`${import.meta.env.VITE_API_URL}/users`)
   return data
 }
 
