a12314371173d4e6a695ecee41e3b0ca2bed0c5d
Dima Mironenko <City7gor@gmail.com>
2026-01-09 17:22:53 +0200

    Add tab client type in window

--- src/api/tanstack.ts
+++ src/api/tanstack.ts
@@ -2,6 +2,8 @@ import { QueryClient } from "@tanstack/vue-query"
 
 const queryClient = new QueryClient()
 
-window.__TANSTACK_QUERY_CLIENT__ = queryClient
+if (import.meta.env.DEV) {
+  window.__TANSTACK_QUERY_CLIENT__ = queryClient
+}
 
 export { queryClient }
