2352a55dab1cf1c67022e8e96f50544d198ae280
Dima Mironenko <City7gor@gmail.com>
2026-01-16 18:40:18 +0200

    Add 2 mutation in users

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts" generic="T extends { id: PropertyKey }">
-import Error from './Error.vue';
-import Spinner from './Spinner.vue';
+import type { FetchStatus } from '@tanstack/vue-query'
+import Error from './Error.vue'
+import Spinner from './Spinner.vue'
 
 const props = defineProps<{
-  items: T[]
-  columns: Partial<Record<keyof T, string>>
-  status: 'pending' | 'error' | 'success'
+  items: T[] | undefined
+  columns: Record<string, string>
+  status: 'pending'| 'success' | 'error'
+  fetchStatus: FetchStatus
 }>()
 
 const columnKeys = Object.keys(props.columns) as (keyof T)[];
@@ -16,14 +18,16 @@ const columnKeys = Object.keys(props.columns) as (keyof T)[];
     <table class="whitespace-nowrap w-full text-sm text-left">
       <thead class="text-sm font-bold uppercase bg-gray-200">
         <tr>
-          <th v-for="(key, value) in columns" :key="key as PropertyKey" class="px-4 py-3">{{ value }}</th>
-          <th class="px-4 py-3 text-right">Actions</th>
+          <th v-for="(key, value) in columns" :key="key" class="px-4 py-3">
+            {{ value }}
+          </th>
         </tr>
       </thead>
+
       <tbody>
         <template v-if="status === 'pending'">
           <tr>
-            <td :colspan="Object.keys(columns).length + 1" class="px-4 py-48">
+            <td :colspan="columnKeys.length" class="px-4 py-48">
               <Spinner />
             </td>
           </tr>
@@ -31,7 +35,7 @@ const columnKeys = Object.keys(props.columns) as (keyof T)[];
 
         <template v-else-if="status === 'error'">
           <tr>
-            <td :colspan="Object.keys(columns).length + 1" class="px-4 py-48">
+            <td :colspan="columnKeys.length" class="px-4 py-48">
               <Error />
             </td>
           </tr>
@@ -43,16 +47,10 @@ const columnKeys = Object.keys(props.columns) as (keyof T)[];
             :key="item.id"
             :class="[ index % 2 ? 'bg-gray-100' : 'bg-gray-50' ]"
           >
-            <td v-for="key in columnKeys" :key="key" class="px-4 py-2" :class="[ key === 'id' ? 'font-medium' :  'text-slate-600' ]">{{ item[key] }}</td>
-            <!-- <td class="px-4 py-2">
-              <span
-                class="inline-flex items-center px-3 py-1 rounded-lg text-xs font-medium text-gray-700 border border-gray-600"
-              >
-                {{ item.email === 'City7gor@gmail.com' ? 'Admin' : 'User' }}
-              </span>
-            </td> -->
-            <td class="px-4 py-2 text-right">
-              <button class="text-blue-700 hover:text-blue-500 font-medium">Edit</button>
+            <td v-for="key in columnKeys" :key="key" class="px-4 py-2" :class="[ key === 'id' ? 'font-medium' :  'text-slate-600' ]">
+              <slot :name="`cell-${String(key)}`" :item="item">
+                {{ item[key] }}
+              </slot>
             </td>
           </tr>
         </template>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
-import { loadUsers } from '@/api'
+import { loadUsers, deleteUser, updateUser } from '@/api'
 import Table from '@/components/Table.vue'
-import { useQuery } from '@tanstack/vue-query'
+import { useMutation, useQuery } from '@tanstack/vue-query'
 import withNotification from '@/helpers/withNotification'
+import Toggle from './Toggle.vue'
 
-const { data: users, status } = useQuery({
+const { data: users, status, fetchStatus, refetch } = useQuery({
   queryKey: ['users'],
-  queryFn: async () => withNotification('users', loadUsers),
+  queryFn: withNotification('users', loadUsers),
+})
+
+const { mutate: toggleAdmin } = useMutation({
+  mutationFn: withNotification('users', updateUser),
+  onSettled: () => refetch(),
+})
+
+const { mutate: removeUser } = useMutation({
+  mutationFn: withNotification('users', deleteUser),
+  onSettled: () => refetch(),
 })
 </script>
 
 <template>
   <div>
-    <Table :items="users ?? []" :columns="{ id: 'ID', name: 'Name', email: 'Email' }" :status />
+    <Table :items="users" :columns="{ id: 'ID', name: 'Name', email: 'Email', admin: 'Admin', action: 'Actions' }" :status :fetchStatus>
+      <template #cell-admin="{ item }">
+        <Toggle :modelValue="item.isAdmin" @update:model-value="toggleAdmin({ id: item.id, isAdmin: !item.isAdmin })" />
+      </template>
+
+      <template #cell-action="{ item }">
+        <button class="text-blue-700 hover:text-blue-500 font-medium">Edit</button>
+        <button @click="removeUser(item.id)" class="text-red-700 hover:text-red-500 font-medium">Delete</button>
+      </template>
+    </Table>
   </div>
 </template>
