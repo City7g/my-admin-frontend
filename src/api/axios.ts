5f631b275ac9857d92ea5451bf17f4c9253fe41e
Dima Mironenko <City7gor@gmail.com>
2026-01-16 18:39:37 +0200

    Add API_DELAY in axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,14 +1,16 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 })
 
+const API_DELAY = Number(import.meta.env.VITE_API_DELAY) || 1000
+
 api.interceptors.request.use(
-  (config) => new Promise(resolve => setTimeout(() => resolve(config), 2000))
+  (config) => new Promise(resolve => setTimeout(() => resolve(config), API_DELAY))
 )
 
 export default api
