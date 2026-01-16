b61699cc0c67ad82990943f479a0784c59e32f85
Dima Mironenko <City7gor@gmail.com>
2026-01-16 17:05:41 +0200

    Update withNotification helper

--- src/helpers/withNotification.ts
+++ src/helpers/withNotification.ts
@@ -1,14 +1,20 @@
-import { useNotificationStore } from "@/stores/notification"
+import { useNotificationStore } from '@/stores/notification'
+import type { NotificationType } from '@/types/Notification'
 
-export default async <T>(name: string, func: () => Promise<T>): Promise<T> => {
+export default <Fn extends (...args: any[]) => Promise<any>>(
+  name: string,
+  func: Fn
+): (...args: Parameters<Fn>) => Promise<Awaited<ReturnType<Fn>>> => {
   const notificationStore = useNotificationStore()
 
-  try {
-    const res = await func()
-    notificationStore.add(`Load ${name}`)
-    return res
-  } catch (error) {
-    notificationStore.add(`Failed load ${name}`, 'error', 'Please, try again')
-    throw error
+  return async (...args: Parameters<Fn>): Promise<Awaited<ReturnType<Fn>>> => {
+    try {
+      const res = await func(...args)
+      notificationStore.add(`Load ${name}`)
+      return res
+    } catch (error) {
+      notificationStore.add(`Failed load ${name}`, 'error', 'Please, try again')
+      throw error
+    }
   }
 }
