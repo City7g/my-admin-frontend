3c6f0b56c18250b5f041acd84e1cec7f2a6fa268
Dima Mironenko <City7gor@gmail.com>
2026-01-16 17:04:17 +0200

    Add methods in users api

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,9 +1,18 @@
-import type { Resource, User } from '@/types'
 import api from './axios'
+import type { Resource, User } from '@/types'
+import type { RequireKey } from '@/types/helpers'
 
 const loadUsers = async () => {
-  const { data } = await api.get<Resource<User>>(`${import.meta.env.VITE_API_URL}/users`)
+  const { data } = await api.get<Resource<User>>(`/users`)
   return data
 }
 
-export { loadUsers }
+const updateUser = async ({ id, ...body }: RequireKey<User, 'id'>) => {
+  await api.patch<Resource<void>>(`/users/${id}`, body)
+}
+
+const deleteUser = async (id: User['id']) => {
+  await api.delete<Resource<void>>(`/users/${id}`)
+}
+
+export { loadUsers, updateUser, deleteUser }
